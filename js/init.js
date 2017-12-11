80d94ead57486aa223c3d38c5c40bb498f3a95b9
yohanurien <34236100+yohanurien@users.noreply.github.com>
2017-12-11 14:52:19 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -3,33 +3,6 @@
 /* Init JS
 /*
 -----------------------------------------------------------------------------------*/
-var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
-
-var countdown = document.getElementById("tiles"); // get tag element
-
-getCountdown();
-
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		 
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		  
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
-}
 
 function pad(n) {
 	return (n < 10 ? '0' : '') + n;
@@ -210,6 +183,90 @@ function pad(n) {
 
 });
 
+/* Countdown */
+<div class="wrap">  
+  <h1>Draft <strong>Countdown</strong></h1>
+
+  <div class="countdown">
+    <div class="bloc-time hours" data-init-value="24">
+      <span class="count-title">Hours</span>
+
+      <div class="figure hours hours-1">
+        <span class="top">2</span>
+        <span class="top-back">
+          <span>2</span>
+        </span>
+        <span class="bottom">2</span>
+        <span class="bottom-back">
+          <span>2</span>
+        </span>
+      </div>
+
+      <div class="figure hours hours-2">
+        <span class="top">4</span>
+        <span class="top-back">
+          <span>4</span>
+        </span>
+        <span class="bottom">4</span>
+        <span class="bottom-back">
+          <span>4</span>
+        </span>
+      </div>
+    </div>
+
+    <div class="bloc-time min" data-init-value="0">
+      <span class="count-title">Minutes</span>
+
+      <div class="figure min min-1">
+        <span class="top">0</span>
+        <span class="top-back">
+          <span>0</span>
+        </span>
+        <span class="bottom">0</span>
+        <span class="bottom-back">
+          <span>0</span>
+        </span>        
+      </div>
+
+      <div class="figure min min-2">
+       <span class="top">0</span>
+        <span class="top-back">
+          <span>0</span>
+        </span>
+        <span class="bottom">0</span>
+        <span class="bottom-back">
+          <span>0</span>
+        </span>
+      </div>
+    </div>
+
+    <div class="bloc-time sec" data-init-value="0">
+      <span class="count-title">Seconds</span>
+
+        <div class="figure sec sec-1">
+        <span class="top">0</span>
+        <span class="top-back">
+          <span>0</span>
+        </span>
+        <span class="bottom">0</span>
+        <span class="bottom-back">
+          <span>0</span>
+        </span>          
+      </div>
+
+      <div class="figure sec sec-2">
+        <span class="top">0</span>
+        <span class="top-back">
+          <span>0</span>
+        </span>
+        <span class="bottom">0</span>
+        <span class="bottom-back">
+          <span>0</span>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 
